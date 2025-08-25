5394e3c6736cad65f0bb24cc13c41e07276c6c5b
avdeshjadon <theavdeshjadon@gmail.com>
2025-08-25 22:13:21 +0530

    added console logs

---
+++ public/script/console-dev.js
@@ -0,0 +1,26 @@
+const styles = [
+  "color: #469154",
+  "font-size: 1.5rem", 
+  "font-weight: bold",
+  "font-family: 'Fira Code', monospace",
+  "text-shadow: 2px 2px 4px rgba(0,0,0,0.2)",
+].join(";");
+
+const nameStyle = [
+  "color: #e8891dff",
+  "font-size: 1.8rem",
+  "font-weight: bold", 
+  "font-family: 'Fira Code', monospace",
+].join(";");
+
+console.clear();
+
+console.log("%c██████╗ ███████╗██╗   ██╗███████╗██╗      ██████╗ ██████╗ ███████╗██████╗ ", "color: #469154; font-weight: bold;");
+console.log("%c██╔══██╗██╔════╝██║   ██║██╔════╝██║     ██╔═══██╗██╔══██╗██╔════╝██╔══██╗", "color: #469154; font-weight: bold;");
+console.log("%c██║  ██║█████╗  ██║   ██║█████╗  ██║     ██║   ██║██████╔╝█████╗  ██████╔╝", "color: #469154; font-weight: bold;");
+console.log("%c██║  ██║██╔══╝  ╚██╗ ██╔╝██╔══╝  ██║     ██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗", "color: #469154; font-weight: bold;");
+console.log("%c██████╔╝███████╗ ╚████╔╝ ███████╗███████╗╚██████╔╝██║     ███████╗██║  ██║", "color: #469154; font-weight: bold;");
+console.log("%c╚═════╝ ╚══════╝  ╚═══╝  ╚══════╝╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝", "color: #469154; font-weight: bold;");
+
+console.log("%cAvdesh Jadon", nameStyle);
+console.log("%cDeveloped with ❤️", styles);
